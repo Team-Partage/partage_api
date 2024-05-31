0f66ee4b7ede0a9205931613ed2c9fbadefb007d
민병욱 <vvxxxxk@gmail.com>
2024-05-31 17:24:24 +0900

    data.sql update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,8 @@ insert into tb_user_role(role_id, role_name) values('R0200', 'ROLE_USER');
 insert into tb_channel_role(role_id, role_name) values('C0000', 'ROLE_OWNER');
 insert into tb_channel_role(role_id, role_name) values('C0100', 'ROLE_MODERATOR');
 insert into tb_channel_role(role_id, role_name) values('C0200', 'ROLE_VIEWER');
+
+-- alter column charset utf8mb4
+ALTER TABLE tb_playlist MODIFY title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE tb_channel MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE tb_user MODIFY nickname VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
