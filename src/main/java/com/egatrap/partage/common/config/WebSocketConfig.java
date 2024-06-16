13868398c1276dc2c11db0c65c87458492891d0f
vvxxxxk <vvxxxxk@gmail.com>
2024-06-16 22:03:18 +0900

    websocket cors test

--- src/main/java/com/egatrap/partage/common/config/WebSocketConfig.java
+++ src/main/java/com/egatrap/partage/common/config/WebSocketConfig.java
@@ -8,6 +8,7 @@ import com.egatrap.partage.service.ChannelService;
 import com.egatrap.partage.service.ChannelUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -20,6 +21,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${security.cors.allowed-origins}")
+    private String ALLOWED_ORIGINS;
+
     private final JwtTokenProvider jwtTokenProvider;
     private final ChannelService channelService;
     private final ChannelPermissionService channelPermissionService;
@@ -29,6 +33,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 //        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // browser only
         registry.addEndpoint("/ws")
+                .setAllowedOrigins(ALLOWED_ORIGINS)
                 .setHandshakeHandler(new WsHandshakeHandler())
                 .withSockJS()
                 .setInterceptors(new SessionAttributesInterceptor(
