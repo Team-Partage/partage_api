63150827fa33eaaedc4343cbaf5581ba74277558
vvxxxxk <vvxxxxk@gmail.com>
2024-07-31 16:42:17 +0900

    Rest API PATCH method allowed config add

--- src/main/java/com/egatrap/partage/common/config/WebSecurityConfig.java
+++ src/main/java/com/egatrap/partage/common/config/WebSecurityConfig.java
@@ -89,7 +89,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(ALLOWED_ORIGINS));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
